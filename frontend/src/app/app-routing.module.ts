a9433d924cf93d2be5eedd5ccf439cbb10f1fa8a
vyvr <lucianocoddingstuff@gmail.com>
2024-02-09 11:03:10 +0100

    updated routing to dashboard children

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './features/auth/auth.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { CanActivateDashboard } from './core/can-activate-dashboard.guard';
+import { PostsComponent } from './features/dashboard/posts/posts.component';
+import { FarmersComponent } from './features/dashboard/farmers/farmers.component';
+import { MenuBookComponent } from './features/dashboard/menu-book/menu-book.component';
+import { MarketComponent } from './features/dashboard/market/market.component';
+import { SettingsComponent } from './features/dashboard/settings/settings.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/auth', pathMatch: 'full' },
@@ -11,6 +16,13 @@ const routes: Routes = [
     path: 'dashboard',
     component: DashboardComponent,
     canActivate: [CanActivateDashboard],
+    children: [
+      { path: 'posts', component: PostsComponent },
+      { path: 'menu-book', component: MenuBookComponent },
+      { path: 'market', component: MarketComponent },
+      { path: 'farmers', component: FarmersComponent },
+      { path: 'settings', component: SettingsComponent },
+    ],
   },
   { path: '**', redirectTo: '/auth' },
 ];
