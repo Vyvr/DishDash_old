3556968736ae0c319ad4001e8f741e4098422ea1
vyvr <lucianocoddingstuff@gmail.com>
2024-02-09 11:14:14 +0100

    creates basic navigation through dashboard children

--- frontend/src/app/features/dashboard/header/header.component.ts
+++ frontend/src/app/features/dashboard/header/header.component.ts
@@ -1,8 +1,35 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'dashboard-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private router: Router) {}
+
+  navigateToPosts() {
+    this.router.navigate(['/dashboard/posts']);
+  }
+
+  navigateToMenuBook() {
+    this.router.navigate(['/dashboard/menu-book']);
+  }
+
+  navigateToFarmers() {
+    this.router.navigate(['/dashboard/farmers']);
+  }
+
+  navigateToMarket() {
+    this.router.navigate(['/dashboard/market']);
+  }
+
+  navigateToSettings() {
+    this.router.navigate(['/dashboard/settings']);
+  }
+
+  logout() {
+    // this.router.navigate(['/dashboard/posts']);
+  }
+}
